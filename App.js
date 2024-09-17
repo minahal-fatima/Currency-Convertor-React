4fe3601b0527eed2333992a5a9086e87cb5e49f2
minahal-fatima <109409510+minahal-fatima@users.noreply.github.com>
2024-09-17 10:51:37 +0500

    Add files via upload
    This React component is a simple currency converter that fetches real-time exchange rates using the Frankfurter API. It allows users to input an amount and select currencies to convert from and to. The component handles user input for the amount and currency selection, displays the converted value, and manages loading states while fetching data asynchronously. If the user selects the same currency for both "from" and "to," the component bypasses the API call and returns the original amount.

---
+++ App.js
@@ -0,0 +1,56 @@
+import './index.css';
+import {useEffect, useState} from 'react';
+
+export default function App(){
+
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function(){
+    async function convert(){
+      setIsLoading(true);
+      const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`);
+      const data = await res.json();
+      setConverted(data.rates[toCur]);
+      setIsLoading(false);
+    } 
+      
+    if(fromCur === toCur) return setConverted(amount);
+    convert();
+  },[amount, fromCur, toCur]);
+
+  return(
+    <div>
+      <input 
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}>
+      </input>
+
+      <select 
+        value={fromCur}
+        onChange={(e) => setFromCur(e.target.value)}
+        disabled={isLoading}>
+        <option value="EUR">EUR</option>
+        <option value="INR">INR</option>
+        <option value="CAD">CAD</option>
+        <option value="USD">USD</option>
+      </select>
+
+      <select
+        value={toCur}
+        onChange={(e) => setToCur(e.target.value)}
+        disabled={isLoading}>
+        <option value="EUR">EUR</option>
+        <option value="INR">INR</option>
+        <option value="CAD">CAD</option>
+        <option value="USD">USD</option>
+      </select>
+      <p>{converted} {toCur}</p>
+    </div>
+  );
+}
